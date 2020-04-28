008ef2bb5d3773cc67c59327566f8e9d5cc57edf
Anna Luchechko <annaluchechko@gmail.com>
2020-04-28 23:53:04 +0300

    Create functions to get box size, origin and draw first box

--- Target/ViewController.swift
+++ Target/ViewController.swift
@@ -12,9 +12,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let boxSize = getBoxSize(widthScreenSide: view.frame.size.width, heightScreenSide: view.frame.size.height)
+        drawBox(boxSize: boxSize, origin: getBoxOrigin(widthScreenSide: view.frame.size.width, heightScreenSide: view.frame.size.height, boxSize: boxSize) )
     }
 
+    func getBoxSize(widthScreenSide:CGFloat, heightScreenSide:CGFloat) -> CGSize {
+        let boxSize = widthScreenSide - 50
+    
+        return CGSize(width: boxSize, height: boxSize)
+    }
+    
+    func getBoxOrigin(widthScreenSide:CGFloat, heightScreenSide:CGFloat, boxSize: CGSize) -> CGPoint {
+        let yOrigin = (heightScreenSide - boxSize.height) / 2
+        let xOrigin = (widthScreenSide - boxSize.width) / 2
+
+        return CGPoint(x: xOrigin, y: yOrigin)
+    }
+    
+    func drawBox(boxSize: CGSize, origin: CGPoint) {
+        let box = UIView()
+        box.frame.size.width = boxSize.width
+        box.frame.size.height = boxSize.width
+        box.frame.origin.x = origin.x
+        box.frame.origin.y = origin.y
+        box.backgroundColor = .magenta
+        self.view.addSubview(box)
+    }
 
 }
-
