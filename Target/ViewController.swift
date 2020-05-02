7c722195cb27324a3fad16e26facc3d1aeb2fdd6
Anna Luchechko <annaluchechko@gmail.com>
2020-05-02 23:48:44 +0300

    Refactor functions to draw target

--- Target/ViewController.swift
+++ Target/ViewController.swift
@@ -12,46 +12,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let boxSize = getBoxSize(widthScreenSide: view.frame.size.width,
-                                 heightScreenSide: view.frame.size.height)
-        
-        let secondBoxSize = getSecondBoxSize(widthScreenSide: view.frame.size.width,
-                                 heightScreenSide: view.frame.size.height)
-        
-        let thirdBoxSize = getThirdBoxSize(widthScreenSide: view.frame.size.width,
-                                 heightScreenSide: view.frame.size.height)
-        
-        drawBox(boxSize: boxSize, origin: getBoxOrigin(widthScreenSide: view.frame.size.width, heightScreenSide: view.frame.size.height, boxSize: boxSize), boxColor: .blue)
-        drawBox(boxSize: secondBoxSize, origin: getBoxOrigin(widthScreenSide: view.frame.size.width, heightScreenSide: view.frame.size.height, boxSize: secondBoxSize), boxColor: .purple )
-        drawBox(boxSize: thirdBoxSize, origin: getBoxOrigin(widthScreenSide: view.frame.size.width, heightScreenSide: view.frame.size.height, boxSize: thirdBoxSize), boxColor: .blue)
+
+        drawTarget(circleCount: 3, widthScreenSide: self.view.frame.size.width, heightScreenSide: self.view.frame.size.height)
     }
 
-    func getBoxSize(widthScreenSide:CGFloat, heightScreenSide:CGFloat) -> CGSize {
-        let boxSize = widthScreenSide - widthScreenSide / 10
-    
-        return CGSize(width: boxSize, height: boxSize)
-    }
-    
-    func getSecondBoxSize(widthScreenSide:CGFloat, heightScreenSide:CGFloat) -> CGSize {
-        let boxSize = widthScreenSide - widthScreenSide / 4
-    
-        return CGSize(width: boxSize, height: boxSize)
-    }
-    
-    func getThirdBoxSize(widthScreenSide:CGFloat, heightScreenSide:CGFloat) -> CGSize {
-        let boxSize = widthScreenSide - widthScreenSide / 2
-    
+    func getBoxSize(widthScreenSide: CGFloat, boxNumber: CGFloat) -> CGSize {
+        let boxSize = widthScreenSide - (widthScreenSide / 10) * boxNumber * 2
         return CGSize(width: boxSize, height: boxSize)
     }
     
     func getBoxOrigin(widthScreenSide:CGFloat, heightScreenSide:CGFloat, boxSize: CGSize) -> CGPoint {
         let yOrigin = (heightScreenSide - boxSize.height) / 2
         let xOrigin = (widthScreenSide - boxSize.width) / 2
-
         return CGPoint(x: xOrigin, y: yOrigin)
     }
     
-    func drawBox(boxSize: CGSize, origin: CGPoint, boxColor: UIColor) {
+    func drawCircle(boxSize: CGSize, origin: CGPoint, boxColor: UIColor) {
         let box = UIView()
         box.frame.size.width = boxSize.width
         box.frame.size.height = boxSize.width
@@ -60,6 +36,21 @@ class ViewController: UIViewController {
         box.backgroundColor = boxColor
         box.layer.cornerRadius = boxSize.width / 2
         self.view.addSubview(box)
+    }
+    
+    func drawTarget(circleCount: Int, widthScreenSide:CGFloat, heightScreenSide:CGFloat) {
+        for index in 0..<circleCount {
+            let boxNumber = CGFloat(index + 1)
+            let boxSize = getBoxSize(widthScreenSide: widthScreenSide, boxNumber: boxNumber)
+            let boxOrigin = getBoxOrigin(widthScreenSide: widthScreenSide, heightScreenSide: heightScreenSide, boxSize: boxSize)
+            var boxColor: UIColor = .magenta
+            if (Int(boxNumber) % 2 == 0) {
+                boxColor = .blue
+            } else {
+                boxColor = .magenta
+            }
+            drawCircle(boxSize: boxSize, origin: boxOrigin, boxColor: boxColor)
+        }
     }
 
 }
